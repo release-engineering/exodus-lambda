978faba427a0939ea890416ae808508fd77f3dc8
Nathan Gillett <ngillett@redhat.com>
2020-02-12 19:11:23 -0500

    Move map_to_s3 environment variables into function
    Moves the formerly global environment variables into the function
    definition, as they weren't needed globaly and posed challenges in
    testing.
    
    This commit also make minor changes to logging messages and adds a
    return response in the event an invalid table item is received from the
    DB query.

--- cdn_lambda/functions/map_to_s3.py
+++ cdn_lambda/functions/map_to_s3.py
@@ -7,32 +7,31 @@ import boto3
 
 LOG = logging.getLogger("map-to-s3-lambda")
 
-# This AWS Lambda function must be deployed with DB_TABLE_NAME and
-# DB_TABLE_REGION environment variables
-
-TABLE_NAME = os.getenv("DB_TABLE_NAME")
-TABLE_REGION = os.getenv("DB_TABLE_REGION")
-
 
 def lambda_handler(event, context):
     _ = context
+
+    # This AWS Lambda function must be deployed with DB_TABLE_NAME and
+    # DB_TABLE_REGION environment variables
+    table_name = os.getenv("DB_TABLE_NAME")
+    table_region = os.getenv("DB_TABLE_REGION")
 
     # Get uri from origin request event
     request = event["Records"][0]["cf"]["request"]
     web_uri = request["uri"]
 
     # Query latest item with matching uri from DynamoDB table
-    LOG.info("Querying '%s' table for '%s'", TABLE_NAME, web_uri)
+    LOG.info("Querying '%s' table for '%s'...", table_name, web_uri)
 
-    query_result = boto3.client("dynamodb", region_name=TABLE_REGION).query(
-        TableName=TABLE_NAME,
+    query_result = boto3.client("dynamodb", region_name=table_region).query(
+        TableName=table_name,
         Limit=1,
         ScanIndexForward=False,
         KeyConditionExpression="web_uri = :u and from_date <= :d",
         ExpressionAttributeValues={
             ":u": {"S": web_uri},
-            ":d": {"S": str(datetime.utcnow())}
-        }
+            ":d": {"S": str(datetime.utcnow())},
+        },
     )
 
     if query_result["Items"]:
@@ -43,15 +42,20 @@ def lambda_handler(event, context):
 
         try:
             request["uri"] = latest_item["object_key"]["S"]
+            return request
         except KeyError:
-            LOG.error("No 'object_key' found in %s", json.dumps(latest_item))
+            err_msg = "No 'object_key' found in %s", json.dumps(latest_item)
+            LOG.error(err_msg)
+            return {
+                "status": "500",
+                "statusDescription": "Internal Server Error",
+                "body": json.dumps({"error": err_msg})
+            }
     else:
         LOG.info("No item for '%s'", web_uri)
 
         # Report 404 to prevent attempts on S3
-        request = {
+        return {
             "status": "404",
             "statusDescription": "Not Found",
         }
-
-    return request
