4f6d61c47857a07719ec6e539fd1ae09fd4c4965
Nathan Gillett <ngillett@redhat.com>
2023-03-22 14:39:21 -0500

    Improve origin response logging [RHELDST-16814]
    Adds additional messages for updated request and response, remove json
    dumps from existing request/response message, leaving formatting to
    logging handler.

--- exodus_lambda/functions/origin_response.py
+++ exodus_lambda/functions/origin_response.py
@@ -1,4 +1,3 @@
-import json
 import os
 from base64 import b64encode
 
@@ -18,12 +17,10 @@ class OriginResponse(LambdaBase):
         response = event["Records"][0]["cf"]["response"]
 
         self.logger.debug(
-            "Original request value for origin_response: %s",
-            json.dumps(request, indent=4, sort_keys=True),
+            "Original request value for origin_response: %s", request
         )
         self.logger.debug(
-            "Original response value for origin_response: %s",
-            json.dumps(response, indent=4, sort_keys=True),
+            "Original response value for origin_response: %s", response
         )
 
         if "headers" in request and "want-digest" in request["headers"]:
@@ -50,6 +47,13 @@ class OriginResponse(LambdaBase):
 
         if original_uri:
             self.set_cache_control(original_uri, response)
+
+        self.logger.debug(
+            "Updated request value for origin_response: %s", request
+        )
+        self.logger.debug(
+            "Updated response value for origin_response: %s", response
+        )
 
         return response
 
