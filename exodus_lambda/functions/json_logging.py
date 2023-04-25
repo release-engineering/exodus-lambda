09311dff1346f28058e3b8b6fdc4bc5a1c7072ab
Nathan Gillett <ngillett@redhat.com>
2023-04-25 15:00:59 -0500

    Logging: record milliseconds in timestamp by default [RHELDST-16619]
    Overrides logging's formatTime to use datetime objects and a period
    separator for msec. Removes the datefmt set in the lambda config template
    but the field remains configurable should the client/service choose.
    All log times are now in UTC timezone.

--- exodus_lambda/functions/json_logging.py
+++ exodus_lambda/functions/json_logging.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import logging
 
@@ -13,8 +14,24 @@ class JsonFormatter(logging.Formatter):
             "request": "request",
             "response": "response",
         }
-        self.datefmt = datefmt or "%Y-%m-%d %H:%M:%S"
-        self.dictfmt = None
+        self.datefmt = datefmt
+
+    # Appended '_' on 'converter' because mypy doesn't approve of
+    # overwriting a base class variable with another type.
+    converter_ = datetime.datetime.fromtimestamp
+
+    default_time_format = "%Y-%m-%d %H:%M:%S"
+    default_msec_format = "%s.%03d"
+
+    def formatTime(self, record, datefmt=None):
+        ct = self.converter_(record.created, datetime.timezone.utc)
+        if datefmt:
+            s = ct.strftime(datefmt)
+        else:
+            s = ct.strftime(self.default_time_format)
+            if self.default_msec_format:
+                s = self.default_msec_format % (s, record.msecs)
+        return s
 
     def formatMessage(self, record):
         return {k: record.__dict__.get(v) for k, v in self.fmt.items()}
--- tests/functions/test_base.py
+++ tests/functions/test_base.py
@@ -1,7 +1,10 @@
+import copy
+import json
 import logging
 import os
 
 import pytest
+from freezegun import freeze_time
 
 from exodus_lambda.functions.base import LambdaBase
 
@@ -9,6 +12,7 @@ from ..test_utils.utils import generate_test_config
 
 CONF_FILE = os.environ.get("EXODUS_LAMBDA_CONF_FILE")
 TEST_CONF = generate_test_config()
+MOCKED_DT = "2023-04-26 14:43:13.570034+00:00"
 
 
 def test_base_handler():
@@ -65,7 +69,44 @@ def test_root_logger_without_handlers(caplog):
     assert "warning message" not in caplog.text
 
 
-def test_json_handler_stack_info(caplog):
+def test_json_logger_stack_info(caplog):
     base_obj = LambdaBase(conf_file=TEST_CONF)
     base_obj.logger.exception("oops", stack_info=True)
     assert '"stack_info": "Stack (most recent call last)' in caplog.text
+
+
+@freeze_time(MOCKED_DT)
+def test_json_logger_timestamp(caplog):
+    LambdaBase(conf_file=TEST_CONF).logger.info("Works!")
+
+    # Logged timestamp should show milliseconds by default
+    assert json.loads(caplog.text) == {
+        "level": "INFO",
+        "time": "2023-04-26 14:43:13.570",
+        "aws-request-id": None,
+        "message": "Works!",
+        "request": None,
+        "response": None,
+    }
+
+
+@freeze_time(MOCKED_DT)
+def test_json_logger_configurable_datefmt(caplog):
+    """Ensure logger's datefmt is configurable"""
+
+    new_datefmt = "%H:%M on %A, %B %d, %Y"
+
+    conf = copy.deepcopy(TEST_CONF)
+    conf["logging"]["formatters"]["default"]["datefmt"] = new_datefmt
+
+    LambdaBase(conf_file=conf).logger.info("Works!")
+
+    # Logged timestamp should be formatted as new_datefmt
+    assert json.loads(caplog.text) == {
+        "level": "INFO",
+        "time": "14:43 on Wednesday, April 26, 2023",
+        "aws-request-id": None,
+        "message": "Works!",
+        "request": None,
+        "response": None,
+    }
